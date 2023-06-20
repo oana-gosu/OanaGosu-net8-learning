3aafa5085d0281a7e324e55ef02db35d0726e7ee
oana-gosu <oana-gosu@users.noreply.github.com>
2023-06-20 18:09:33 +0300

    u

---
+++ C#intro/C#intro/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(  );
+Console.WriteLine("Un alt text");
+int number = 7;
+int anothernumber = 7 + 2;
+Console.WriteLine(number);
+Console.WriteLine(anothernumber);
+
+

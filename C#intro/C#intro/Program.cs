47181b5dd31f682382928acf81c246a19479f886
oana-gosu <oana-gosu@users.noreply.github.com>
2023-06-27 21:00:13 +0300

    Update Program.cs

--- C#intro/C#intro/Program.cs
+++ C#intro/C#intro/Program.cs
@@ -117,66 +117,113 @@ int anothernumber = 7 + 2;
 //        }
 //}
 
-int[] numbers = new int[5];
-numbers[0] = 12;
-numbers[1] = 14;
-numbers[2] = 34;
-numbers[3] = 567;
-numbers[4] = 34;
-Console.WriteLine(numbers[1]);
-int sum = numbers[0] + numbers[1] + numbers[2];
-int prod = numbers[3] * numbers[4];
-Console.WriteLine("sum =" + sum);
-Console.WriteLine("prod = " + prod);
-Console.WriteLine("sum2 = " + (sum + prod));
+//int[] numbers = new int[5];
+//numbers[0] = 12;
+//numbers[1] = 14;
+//numbers[2] = 34;
+//numbers[3] = 567;
+//numbers[4] = 34;
+//Console.WriteLine(numbers[1]);
+//int sum = numbers[0] + numbers[1] + numbers[2];
+//int prod = numbers[3] * numbers[4];
+//Console.WriteLine("sum =" + sum);
+//Console.WriteLine("prod = " + prod);
+//Console.WriteLine("sum2 = " + (sum + prod));
 
-List<int> numere = new List<int>();
-numere.Add(12);
-numere.Add(14);
-numere.Add(34);
-numere.Add(567);
-numere.Add(34);
-Console.WriteLine(numere[1]);
+//List<int> numere = new List<int>();
+//numere.Add(12);
+//numere.Add(14);
+//numere.Add(34);
+//numere.Add(567);
+//numere.Add(34);
+//Console.WriteLine(numere[1]);
 
-List<string> nume = new List<string> { "Ana", "Victor", "Luca" };
-Console.WriteLine(nume[0] + "," + nume[1] + "," + nume[2]);
+//List<string> nume = new List<string> { "Ana", "Victor", "Luca" };
+//Console.WriteLine(nume[0] + "," + nume[1] + "," + nume[2]);
 
-Queue<int> coada = new Queue<int>();
-coada.Enqueue(12);
-coada.Enqueue(14);
-coada.Enqueue(34);
-coada.Enqueue(567);
-coada.Enqueue(34);
-Console.WriteLine(coada.ElementAt(1));
+//Queue<int> coada = new Queue<int>();
+//coada.Enqueue(12);
+//coada.Enqueue(14);
+//coada.Enqueue(34);
+//coada.Enqueue(567);
+//coada.Enqueue(34);
+//Console.WriteLine(coada.ElementAt(1));
 
-Stack<int> stiva = new Stack<int>(5);
-stiva.Push(12);
-stiva.Push(14);
-stiva.Push(34);
-stiva.Push(567);
-stiva.Push(34);
-Console.WriteLine(stiva.ElementAt(1));
+//Stack<int> stiva = new Stack<int>(5);
+//stiva.Push(12);
+//stiva.Push(14);
+//stiva.Push(34);
+//stiva.Push(567);
+//stiva.Push(34);
+//Console.WriteLine(stiva.ElementAt(1));
 
-Dictionary<int, string> note = new Dictionary<int, string>();
-note.Add(7, "Ana");
-note.Add(5, "Nicu");
-note.Add(8, "Iulia");
-note.Add(10, "Maria");
-note.Add(9, "Simona");
-Console.WriteLine(note[5]);
+//Dictionary<int, string> note = new Dictionary<int, string>();
+//note.Add(7, "Ana");
+//note.Add(5, "Nicu");
+//note.Add(8, "Iulia");
+//note.Add(10, "Maria");
+//note.Add(9, "Simona");
+//Console.WriteLine(note[5]);
 
-Dictionary<int, List<string>> note2 = new Dictionary<int, List<string>>();
-note2.Add(10, new List<string> { "Ana", "Maria" });
-note2.Add(8, new List<string> { "Ileana", "Viorel", "Gicu" });
-note2.Add(7, new List<string> { "Ioan", "Iulia" });
+//Dictionary<int, List<string>> note2 = new Dictionary<int, List<string>>();
+//note2.Add(10, new List<string> { "Ana", "Maria" });
+//note2.Add(8, new List<string> { "Ileana", "Viorel", "Gicu" });
+//note2.Add(7, new List<string> { "Ioan", "Iulia" });
 
-Console.WriteLine(note2[10][0]);
-Console.WriteLine(note2[8][1]);
+//Console.WriteLine(note2[10][0]);
+//Console.WriteLine(note2[8][1]);
 
+//for (int i = 2; i<=15; )
+//{
+//    Console.WriteLine(i);
+//    i = i + 2;
+//}
 
+//for (int i = 14; i>=2; i-=2 )
+//{
+//    Console.WriteLine(i);
+//}
 
+//List<int> list = new List<int>();
+//for (int i = -100; i<=100; i++)
+//{
+//    if (i % 7 == 0)
+//    {
+//        list.Add(i);
+//        Console.WriteLine(i);
+//    }
+//}
 
+//List<int> numere = new List<int>();
+//for (int i = 1; i<=5; i++)
+//{
+//    Console.WriteLine("Input no:");
+//    int j = Convert.ToInt32(Console.ReadLine());
+//    numere.Add(j);
+//}
+//foreach (int element in numere)
+//{
+//    Console.WriteLine(element*12.75);
+//}
 
+//int x = -100;
+//while (x<=100)
+//{ if (x % 7 == 0)
+//    {
+//        Console.WriteLine(x);
+       
+//    }
+//    x++;
+//}
 
+int z = 56;
+do
+{
+    if (z % 24 == 0)
+    {
+        Console.WriteLine(z);
+    }
+    z++;
+} while (z <= 987);
 
 
